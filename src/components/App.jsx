cdbbd7403aaaaa4aa26ee34d7caba9c5c8790f88
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-02-08 13:54:40 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import { Profile } from "./Profile/Profile";
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile />
   );
 };
