254aded9f2922bca8e101a3abf6d00d2532af010
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-02-26 17:12:19 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-const element = React.createElement('div')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
